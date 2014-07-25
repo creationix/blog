fa9082b0070a997bc2eae381eba3618a65577d66
Tim Caswell <tim@creationix.com>
2014-07-25 18:51:11 -0500

    test

--- template/layout.js
+++ template/layout.js
@@ -1,5 +1,7 @@
+var slice = [].slice;
 
-module.exports = function (title, body) {
+module.exports = function (title) {
+  var body = slice.call(arguments, 1);
   return ["html", { lang: "en" },
     ["head",
       ["title", title]
